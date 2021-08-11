4e97fbdc0ecf482adf90b3aeeeb7f25696b33dfe
chrisintae <hello@chrisintae.com>
2021-08-10 20:04:32 -0400

    Update index with api.js

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,20 +2,21 @@ import Link from 'next/link'
 
 import styles from '../styles/components/grid.module.scss'
 
-import { PostContent } from "../lib/posts";
+// import { PostContent } from "../lib/posts";
 
 type Props = {
-    projects: PostContent[];
+    projects: [];
 }
 
 const Grid = ({ projects }: Props) => {
     return (
         <section className={`wrapper ${styles.grid}`}>
-            {projects.slice(0, 4).map((post, i) => (
+            {projects.slice(0, 4).map(({ title, type, slug }, i) => (
                 <Card 
                 key={i}
-                title={post.title}
-                slug={post.slug}
+                title={title}
+                type={type}
+                slug={slug}
                 />
             ))}
         </section>
@@ -25,15 +26,15 @@ const Grid = ({ projects }: Props) => {
 export default Grid
 
 
-const Card = ({ title, slug }) => {
+const Card = ({ title, type, slug }) => {
     return (
         <div className={styles.card}>
-            <Link href={"/posts/" + slug}>
+            <Link href={`/projects/${slug}`}>
                 <a>
                     <div className={styles.container}>
                         <div className={styles.inner}>
                             <h2>{title}</h2>
-                            <h4>Project Type</h4>
+                            <h4>{type}</h4>
                         </div>
                     </div>
                 </a>
